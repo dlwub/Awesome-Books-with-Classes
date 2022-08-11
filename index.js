02ff8e4d681996f0f95aaf080170483c11b7e706
dlwub <dlwubi@gmail.com>
2022-08-11 06:43:43 +0300

    Fix linter errors in index.js file

--- index.js
+++ index.js
@@ -4,96 +4,82 @@ const authorForm = bookForm.elements['author-input'];
 const bookList = document.getElementById('books-list');
 
 class Book {
-	booksArray = []
+  booksArray = [];
 
-	constructor() {
-		this.getLocalStorage()		
-	}
+  constructor() {
+    this.getLocalStorage();
+  }
 
-	inArray(id) {
-		for (let book of this.booksArray) {
-			if (book.Title.includes(id)) {
-				return true;
-			}
-			return false;
-		}
-	}
+  inArray(id) {
+    this.booksArray.forEach((book) => {
+      if (book.Title.includes(id)) {
+        return true;
+      }
+      return false;
+    });
+  }
 
-	addBookToArray(title, author) {
-		//Check for empty book and add book to booksArray 
-		if (title && author) {
-			if (!this.inArray(title)) {
-				this.booksArray = [...this.booksArray, {
-					"id": title,
-					"Title": title,
-					"Author": author
-				}];
-			}
-		}		
-	}
+  addBookToArray(title, author) {
+    // Check for empty book and add book to booksArray
+    if (title && author) {
+      if (!this.inArray(title)) {
+        this.booksArray = [...this.booksArray, {
+          id: title,
+          Title: title,
+          Author: author,
+        }];
+      }
+    }
+  }
 
-	createElt(elt, clsName, textContent = '') {
-		let elt1 = document.createElement(elt);
-		elt1.clsName = clsName;
-		if (textContent !== "") {
-			elt1.textContent = textContent;
-		}
-		return elt1;
-	}
+  addToPage = (book) => {
+    this.addToPage = this.addToPage.bind(this);
+    bookList.innerHTML += `<tr class="table-row" >
+    <td class="title-col" >"${book.Title}" by ${book.Author}</td>
+    <td class="btn-col"><button id="${book.Title}" type="submit" onclick= book.removeBook(this.id)>Remove</button></td>
+    </tr>`;
+  };
 
-	displayPage() {
-		bookList.innerHTML = '';
-		this.booksArray.forEach(this.addToPage);		
-	}
+  displayPage() {
+    bookList.innerHTML = '';
+    this.booksArray.forEach(this.addToPage);
+  }
 
-	addToPage(book) {
-	
-		bookList.innerHTML += `
-		<tr class="table-row" >
-			<td class="title-col" >"${book.Title}" by ${book.Author}</td>
-			<td class="btn-col"><button id="${book.Title}" type="submit" onclick= book.removeBook(this.id)>Remove</button></td>
-		</tr>
-		`
-	
-	}
+  // Update booksArray with data from localStorage
+  getLocalStorage() {
+    // Check if data is in storage and convert to js object
+    if (localStorage.getItem('bookList')) {
+      this.booksArray = JSON.parse(localStorage.getItem('bookList'));
+    }
+  }
 
-	// Update booksArray with data from localStorage
-	getLocalStorage() {
-		// Check if data is in storage and convert to js object
-		if (localStorage.getItem('bookList')) {
-			this.booksArray = JSON.parse(localStorage.getItem('bookList'));
-		}
-	}
+  setLocalStorage() {
+    localStorage.setItem('bookList', JSON.stringify(this.booksArray));
+  }
 
-	setLocalStorage() {
-		localStorage.setItem('bookList', JSON.stringify(this.booksArray));
-	}
-
-	removeBook(title) {
-		this.booksArray = this.booksArray.filter((book) => book.Title !== title);
-		bookList.innerHTML = '';
-		this.setLocalStorage('bookList', this.booksArray);
-		this.displayPage()
-	}
+  removeBook(title) {
+    this.booksArray = this.booksArray.filter((book) => book.Title !== title);
+    bookList.innerHTML = '';
+    this.setLocalStorage('bookList', this.booksArray);
+    this.displayPage();
+  }
 }
 
-const book = new Book()
+const book = new Book();
 
-window.addEventListener('load', (e) => {
-	book.displayPage();
+window.addEventListener('load', () => {
+  book.displayPage();
 });
 
 bookForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const title = titleForm.value;
-	const author = authorForm.value;
+  e.preventDefault();
+  const title = titleForm.value;
+  const author = authorForm.value;
 
-	book.addBookToArray(title, author);
-	book.setLocalStorage()
-	book.displayPage();
+  book.addBookToArray(title, author);
+  book.setLocalStorage();
+  book.displayPage();
 
-	titleForm.value = "";
-	authorForm.value = "";
+  titleForm.value = '';
+  authorForm.value = '';
 });
-
-// document.querySelector('.table-row').style.backgroundColor = 'red';
